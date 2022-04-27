bf6b64f52f3108672b6212b5804f574cefe0ab3e
Benis <53756956+YellowSubmarine2000@users.noreply.github.com>
2022-04-27 18:17:26 +0600

    Update ToDoEdit.jsx

--- src/components/ToDoEdit.jsx
+++ src/components/ToDoEdit.jsx
@@ -3,9 +3,23 @@ import React, {useState} from 'react'
 const ToDoEdit = () => {
     
     const [hours,setHours] = useState(0)
+    const [minutes,setMinutes] = useState(0)
 
-    function changeTime(i) {
+    function changeHours(i) {
         setHours(hours + i);
+    }
+    function limH(i) {
+        if (i>12) i=12;
+        if (i<0) i=0;
+        return(i)
+    }
+    function limM(i) {
+        if (i>59) i=59;
+        if (i<0) i=0;
+        return(i)
+    }
+    function changeMinutes(i) {
+        setMinutes(minutes + i);
     }
     return (
         <section className="ToDoEdit">
@@ -14,18 +28,18 @@ const ToDoEdit = () => {
             <div className="Time">
                 
                 <div className="hours">
-                    <button className="arrow_up" onClick={() => changeTime(+1)}></button>
-                    <input className="hours_input">{hours}</input>
-                    <button className="arrow_down" onClick={() => changeTime(-1)}></button>
+                    <button className="arrow_up" onClick={() => changeHours(+1)}></button>
+                    <input className="hours_input" value={limH(hours)}></input>
+                    <button className="arrow_down" onClick={() => changeHours(-1)}></button>
                 </div>
                 
                 <span className="dot"></span>
                 <span className="dot"></span>
                 
                 <div className="minutes">
-                    <button className="arrow_up"></button>
-                    <input className="minutes_input"></input>
-                    <button className="arrow_down"></button>
+                    <button className="arrow_up" onClick={() => changeMinutes(1)}></button>
+                    <input className="minutes_input" value={limM(minutes)}></input>
+                    <button className="arrow_down" onClick={() => changeMinutes(-1)}></button>
                 </div>
                 
             </div>
